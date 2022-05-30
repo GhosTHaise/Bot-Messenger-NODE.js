6a47b9e8ae3be9a1e4049647ed7f848b064147ea
GhosTHaise <Ghostrex2@gmail.com>
2022-05-30 21:55:43 +0300

    Add webhook and verfied

--- src/Routes/web.js
+++ src/Routes/web.js
@@ -1,11 +1,12 @@
 import express from "express"
 import homePageController from "../Controllers/homePageController";
-
+import chatBotController from "../Controllers/chatBotController";
 const Router = express.Router();
 
 const initWebRoute = (app) => {
-    Router.get("/",homePageController.getHomePage)
-
+    Router.get("/",homePageController.getHomePage);
+    Router.get("/webhook",chatBotController.getWebhook);
+    Router.post("/webhook",chatBotController.postWebhook);
     //->define the root
     return app.use("/",Router);
 
