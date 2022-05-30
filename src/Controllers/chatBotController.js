4e5cbc5f31428d433f07594beeea1f3552c03c0e
GhosTHaise <Ghostrex2@gmail.com>
2022-05-30 21:32:18 +0300

    Add webHook verification

--- src/Controllers/chatBotController.js
+++ src/Controllers/chatBotController.js
@@ -1,25 +1,50 @@
-let postWebhook = (req,res) => {
-      // Parse the request body from the POST
-  let body = req.body;
+const postWebhook = (req,res) => {
+    let body = req.body;
 
-  // Check the webhook event is from a Page subscription
+    // Checks this is an event from a page subscription
+    if (body.object === 'page') {
+  
+      // Iterates over each entry - there may be multiple if batched
+      body.entry.forEach(function(entry) {
+  
+        // Gets the message. entry.messaging is an array, but 
+        // will only ever contain one message, so we get index 0
+        let webhook_event = entry.messaging[0];
+        console.log(webhook_event);
+      });
+  
+      // Returns a '200 OK' response to all requests
+      res.status(200).send('EVENT_RECEIVED');
+    } else {
+      // Returns a '404 Not Found' if event is not from a page subscription
+      res.sendStatus(404);
+    }
+}
+//ajouter verification webhook
+const getWebhook = (req,res) => {
+    let body = req.body;
+
+  // Checks this is an event from a page subscription
   if (body.object === 'page') {
 
-    // Iterate over each entry - there may be multiple if batched
+    // Iterates over each entry - there may be multiple if batched
     body.entry.forEach(function(entry) {
 
-      // Get the webhook event. entry.messaging is an array, but 
-      // will only ever contain one event, so we get index 0
+      // Gets the message. entry.messaging is an array, but 
+      // will only ever contain one message, so we get index 0
       let webhook_event = entry.messaging[0];
       console.log(webhook_event);
-      
     });
 
-    // Return a '200 OK' response to all events
+    // Returns a '200 OK' response to all requests
     res.status(200).send('EVENT_RECEIVED');
-
   } else {
-    // Return a '404 Not Found' if event is not from a page subscription
+    // Returns a '404 Not Found' if event is not from a page subscription
     res.sendStatus(404);
   }
+}
+
+module.exports = {
+    postWebhook : postWebhook,
+    getWebhook : getWebhook
 }
