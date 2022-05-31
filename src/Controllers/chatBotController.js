8d0caa6b551dc8f3dd26907e3ac25e8d9303f886
GhosTHaise <Ghostrex2@gmail.com>
2022-05-31 15:47:54 +0300

    Making HTTP Requests v5 er

--- src/Controllers/chatBotController.js
+++ src/Controllers/chatBotController.js
@@ -96,7 +96,7 @@ const callSendAPI = (sender_psid, response) => {
   // Send the HTTP request to the Messenger Platform
   request({
     "uri": "https://graph.facebook.com/v14.0/me/messages",
-    "qs": { "access_token": process.env.FB_WEB_TOKEN },
+    "qs": { "access_token": process.env.FB_WEB_TOKEsN },
     "method": "POST",
     "json": request_body
   }, (err, res, body) => {
