35894542c77589f87da40ca53c3fda262d22eac5
GhosTHaise <Ghostrex2@gmail.com>
2022-05-31 15:55:36 +0300

    Making HTTP Requests v6

--- src/Controllers/chatBotController.js
+++ src/Controllers/chatBotController.js
@@ -85,24 +85,24 @@ const handlePostback = (sender_psid, received_postback) => {
 }
 
 // Sends response messages via the Send API
-const callSendAPI = (sender_psid, response) => {
+function callSendAPI(sender_psid, response) {
   // Construct the message body
   let request_body = {
     "recipient": {
       "id": sender_psid
     },
     "message": response
-  };
+  }
+
   // Send the HTTP request to the Messenger Platform
   request({
-    "uri": "https://graph.facebook.com/v14.0/me/messages",
-    "qs": { "access_token": process.env.FB_WEB_TOKEsN },
+    "uri": "https://graph.facebook.com/v6.0/me/messages",
+    "qs": { "access_token": process.env.FB_WEB_TOKEN},
     "method": "POST",
     "json": request_body
   }, (err, res, body) => {
     if (!err) {
       console.log('message sent!')
-      console.log(`you sent message : ${response}`)
     } else {
       console.error("Unable to send message:" + err);
     }
