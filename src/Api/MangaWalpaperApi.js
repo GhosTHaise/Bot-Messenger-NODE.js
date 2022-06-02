3c9763ab768ac393eed4dc529d3891dbd22ca737
GhosTHaise <Ghostrex2@gmail.com>
2022-06-02 12:52:04 +0300

    Add Manga walpaper api axios function fixed 3

--- src/Api/MangaWalpaperApi.js
+++ src/Api/MangaWalpaperApi.js
@@ -7,7 +7,17 @@ const options = {
     'X-RapidAPI-Key': 'b10adcce97msh9638a9ae812c8aep106842jsn5c5e7b510b95'
   }
 };
+let axiosResponse;
 const getDatafromApi = (message) => {
+axios.request(options).then(function (response) {
+	axiosResponse = response.data;
+}).catch(function (error) {
+	console.error(error);
+});
+for(let categorie in axiosResponse[0]){
+    console.log(categorie);
+    console.log(axiosResponse[0])
+}
 let apiResult = [{
     "title": message,
     "subtitle": "Tap a button to answer.",
