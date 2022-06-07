063235c4292faad21734e8495a4a3eece3091fbc
GhosTHaise <Ghostrex2@gmail.com>
2022-06-07 16:41:43 +0300

    add google calendar api fix features 3

--- src/Api/calendarApi.js
+++ src/Api/calendarApi.js
@@ -4,6 +4,7 @@ const {google} = require('googleapis');
 
 const schedule = (sender_psid,callback) => {
     // If modifying these scopes, delete token.json.
+    callback("je vai vous envoyer l'emploie du temps")
 const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
