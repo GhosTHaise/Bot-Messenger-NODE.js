21985c91da662df4322398eaf379f308ed354f52
GhosTHaise <Ghostrex2@gmail.com>
2022-06-07 22:24:59 +0300

    transforme request to asynchrone fix 3

--- src/Controllers/chatBotController.js
+++ src/Controllers/chatBotController.js
@@ -108,9 +108,9 @@ const handleMessage = async (sender_psid, received_message) => {
     }
     if(received_message.text == "Developer"){
      responseText("Do you know me ? I am the GhosT !").then(()=>{
-       setTimeout(responseText("See my work on : https://github.com/GhosTHaise").then(()=>{
-        console.log("get my work succesful");
-      }),1500);
+       setTimeout(()=>{
+        responseText("See my work on : https://github.com/GhosTHaise")
+       },1500);
      }) 
     }
     // Create the payload for a basic text message
