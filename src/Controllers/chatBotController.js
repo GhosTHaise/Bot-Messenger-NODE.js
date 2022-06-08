8bd0f7acd29187695a51337451595e494288a1e3
GhosTHaise <Ghostrex2@gmail.com>
2022-06-08 21:54:02 +0300

    Add quick reply fix3

--- src/Controllers/chatBotController.js
+++ src/Controllers/chatBotController.js
@@ -242,7 +242,22 @@ const quickReply = (sender_psid,ArrayofValue,text) => {
         id: sender_psid
       },
       messaging_type: "RESPONSE",
-      message: Quickresponse
+      message: {
+        "text": "Pick a color:",
+        "quick_replies":[
+          {
+            "content_type":"text",
+            "title":"Red",
+            "payload":"<POSTBACK_PAYLOAD>",
+            "image_url":"http://example.com/img/red.png"
+          },{
+            "content_type":"text",
+            "title":"Green",
+            "payload":"<POSTBACK_PAYLOAD>",
+            "image_url":"http://example.com/img/green.png"
+          }
+        ]
+      }
     }
     // Send the HTTP request to the Messenger Platform
     request({
